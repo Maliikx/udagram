8b4df1460da4a0fd2ed1822bcc5776b3743fbfc9
Amir-Adel9 <amiradel7152@gmail.com>
2023-09-17 15:44:44 +0300

    Switched code to dev branch

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        content: 'var(--color-content)',
+      },
+    },
+  },
+  plugins: [],
+};
