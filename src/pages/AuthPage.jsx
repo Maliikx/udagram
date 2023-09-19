d244a86ac282640aadcb0b8153f7a806a4b8b7d5
Amir-Adel9 <amiradel7152@gmail.com>
2023-09-19 16:18:37 +0300

    Finished register validation

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React from 'react';
 
-import SignIn from "../components/auth/SignIn";
+import SignIn from '../components/auth/SignIn';
 
-import SignUp from "../components/auth/SignUp";
+import SignUp from '../components/auth/SignUp';
 
-import { useState } from "react";
+import { useState } from 'react';
 
 const Auth = () => {
-  const [authState, setAuthState] = useState("login"); // ['login', 'register']
+  const [authState, setAuthState] = useState('register'); // ['login', 'register']
 
   return (
-    <section className="w-full min-h-screen relative flex flex-col justify-center items-center text-center">
-      {authState === "login" ? <SignIn setAuthState={setAuthState}/> : <SignUp setAuthState={setAuthState}/>}
+    <section className='w-full min-h-screen relative flex flex-col justify-center items-center text-center'>
+      {authState === 'login' ? (
+        <SignIn setAuthState={setAuthState} />
+      ) : (
+        <SignUp setAuthState={setAuthState} />
+      )}
     </section>
   );
 };
