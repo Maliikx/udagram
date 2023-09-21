33b49d95d25f8e0b0305c0ce51f1ba17b123a2b2
SheroukYasser <yassersherouk9@gmail.com>
2023-09-21 17:26:00 +0300

    Started with post functionality

--- src/components/home/PostInput.jsx
+++ src/components/home/PostInput.jsx
@@ -1,14 +1,33 @@
 import { Image } from 'lucide-react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const PostInput = () => {
+const PostInput = (props) => {
+  const[postContent , setPostContent] = useState('')
+  const [loggedInUser, setLoggedInUser] = useState()
+
+  useEffect(()=>{
+    setLoggedInUser(JSON.parse(localStorage.getItem("loggedInUser")))
+  },[])
+
+  function createPost(){
+props.setPosts((prevPosts)=> [...prevPosts, {
+  id: prevPosts.length + 1,
+  content: postContent,
+  user: loggedInUser,
+  comments: [],
+  sharedUserIds: [],
+  createdAt: Date.now()
+}])
+  }
   return (
     <div className='bg-accent p-3 border-blue-700 border-x-2 rounded-tl-none rounded-br-none rounded-3xl text-content w-full flex flex-col gap-10'>
       <div className='flex gap-2 items-center'>
         <img src='/assets/cat.jpg' alt='' className='w-12 h-12 rounded-full' />
-        <input
+        <input onChange={(event)=>{
+          setPostContent(event.target.value)
+        }}
           type='text'
-          className='bg-transparent outline-none '
+          className='bg-transparent outline-none w-full '
           placeholder='Type some text...'
         />
       </div>
@@ -17,7 +36,8 @@ const PostInput = () => {
           <Image className='text-content' />
           <span>Upload an image</span>
         </button>
-        <button className='text-white font-bold bg-blue-700 px-5 rounded duration-300 '>
+        <button onClick={createPost}
+        className='text-white font-bold bg-blue-700 px-5 rounded duration-300 '>
           Post
         </button>
       </div>
