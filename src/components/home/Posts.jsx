31c6f7e7e51952fe89c8400c277e8e686fb0fcbe
Amir-Adel9 <amiradel7152@gmail.com>
2023-09-23 11:24:13 +0300

    Posts

--- src/components/home/Posts.jsx
+++ src/components/home/Posts.jsx
@@ -1,16 +1,7 @@
-import React, { useEffect, useState } from "react";
-import Post from "./Post";
-import PostInput from "./PostInput";
-
-import { useRef } from "react";
-
-export const useIsMount = () => {
-  const isMountRef = useRef(true);
-  useEffect(() => {
-    isMountRef.current = false;
-  }, []);
-  return isMountRef.current;
-};
+import React, { useEffect, useState } from 'react';
+import Post from './Post';
+import PostInput from './PostInput';
+import useIsMount from '../../utils/hooks/useMount';
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
@@ -18,20 +9,20 @@ const Posts = () => {
 
   useEffect(() => {
     if (isMount) return;
-    localStorage.setItem("posts", JSON.stringify(posts));
+    localStorage.setItem('posts', JSON.stringify(posts));
   }, [posts]);
 
   useEffect(() => {
-    if (!localStorage.getItem("posts")) {
-      localStorage.setItem("posts", JSON.stringify([]));
+    if (!localStorage.getItem('posts')) {
+      localStorage.setItem('posts', JSON.stringify([]));
     } else {
-      setPosts(JSON.parse(localStorage.getItem("posts")));
+      setPosts(JSON.parse(localStorage.getItem('posts')));
     }
   }, []);
 
   return (
     <>
-      <div className=" rounded flex flex-col gap-6 w-full overflow-y-scroll no-scrollbar ">
+      <div className=' rounded flex flex-col gap-6 w-full overflow-y-scroll no-scrollbar '>
         <PostInput setPosts={setPosts} posts={posts} />
         {posts
           .sort((a, b) => b.createdAt - a.createdAt)
