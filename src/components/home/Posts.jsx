899031c911641c9a8b38348b77ee44cca1cdb1f8
SheroukYasser <yassersherouk9@gmail.com>
2023-09-24 10:29:42 +0300

    nothing to show div

--- src/components/home/Posts.jsx
+++ src/components/home/Posts.jsx
@@ -19,15 +19,18 @@ const Posts = () => {
       setPosts(JSON.parse(localStorage.getItem("posts")));
     }
   }, []);
-
+  
   return (
     <div className=" rounded flex flex-col gap-6 w-full overflow-y-scroll no-scrollbar ">
       <PostInput setPosts={setPosts} posts={posts} />
-      {posts
+      {
+        posts.length === 0 ? <div className="self-center">- Nothing to show -</div> : posts
         .sort((a, b) => b.createdAt - a.createdAt)
         .map((post) => (
           <Post post={post} key={post.id} />
-        ))}
+        ))
+      }
+      
     </div>
   );
 };
