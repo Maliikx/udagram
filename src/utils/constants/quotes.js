6ad280acf4e8d5c8af29aa6ea6786b9ec67619dd
Amir-Adel9 <amiradel7152@gmail.com>
2023-09-26 01:29:32 +0300

    Added quotes & Started Bio

---
+++ src/utils/constants/quotes.js
@@ -0,0 +1,12 @@
+export const quotes = [
+  "Believe you can and you're halfway there.",
+  'The future belongs to those who believe in the beauty of their dreams.',
+  "The harder you work, the greater you'll feel when you achieve.",
+  "Don't watch the clock; keep going.",
+  'The only limit is your doubt.',
+  "You miss 100% of the shots you don't take.",
+  'Be better than you were yesterday.',
+  'Success is the sum of small efforts repeated day in and day out.',
+  'Dream big, work hard, stay focused.',
+  'The only way to do great work is to love what you do.',
+];
