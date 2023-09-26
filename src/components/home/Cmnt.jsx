49da065fef89e55a1d692a4719508f4aad653449
maliikx <eljuoker@gmail.com>
2023-09-26 10:29:54 +0300

    comment early logic

--- src/components/home/Cmnt.jsx
+++ src/components/home/Cmnt.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Cmnt = (props) => {
   return (
       <div className='text-content w-full bg-secondary rounded-br-none  rounded-xl p-1 outline-none'>
+          <div>{props.cmnt.user.username}</div>
           <span>
-              {props.cmnt}
+              {props.cmnt.content}
           </span>
     </div>
   )
